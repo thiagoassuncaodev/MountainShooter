32eca4f8be736eff0494f52c58e785c9f81f803e
thiagodev <thiagoevil3092@gmail.com>
2026-03-06 13:35:43 -0300

    remove venv do repositorio

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+
+print('setup start')
+pygame.init()
+window = pygame.display.set_mode(size=(600, 480)) 
+print('setup end')
+
+
+print('loop start')
+while True :
+    #check for all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
